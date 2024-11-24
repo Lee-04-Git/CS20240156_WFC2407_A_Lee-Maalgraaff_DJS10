56befe20680d1191caa90d7a9c01cc18481a3156
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-11-24 12:06:02 +0200

    added useEffect to fetch posts and handle API errors.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,29 @@ import errorImage from './assets/error-message.png';
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
-  return <div></div>;
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  
+        if (!response.ok) {
+          throw new Error('Failed to fetch blog posts. Please try again later.');
+        }
+  
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+  
+    fetchPosts();
+  }, []);
+
+  return <div>
+
+  </div>;
 };
 
 export default App;
